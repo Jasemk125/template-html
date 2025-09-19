edd080912076f89302634e873a223fdbf07287ea
Jasemk125 <jasemk125@gmail.com>
2025-09-19 22:20:41 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// main.js
+
+// Function to load HTML content into a container
+function loadHTML(containerId, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(containerId).innerHTML = data;
+        })
+        .catch(error => {
+            console.error("Error loading HTML file:", error);
+        });
+}
+
+// Load the header (including navigation) and footer dynamically
+window.onload = function () {
+    loadHTML('header-container', 'header.html');
+    loadHTML('footer-container', 'footer.html');
+};
